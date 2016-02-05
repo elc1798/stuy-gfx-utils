8b8bb7f5fc258b7edbf9dd2f0b639fe141d331e4
elc1798 <subliminalmau5@gmail.com>
2016-02-04 22:10:19 -0500

    Finish assignment

--- picmaker.c
+++ picmaker.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 #include "gfx_utils/fout.h"
 #include "gfx_utils/netpbm.h"
@@ -11,8 +15,8 @@
 
 void fill_rect(pixel **pic, int r, int g, int b, int x, int y, int dx, int dy) {
     int i,j;
-    for (i = x; i < dx; i++) {
-        for (j = y; j < dy; j++) {
+    for (i = x; i < XRES && i < x + dx; i++) {
+        for (j = y; j < YRES && j < y + dy; j++) {
             pic[i][j] = new_pixel(r, g, b);
         }
     }
@@ -25,8 +29,38 @@ int main() {
     int i; for (i = 0; i < XRES; i++) {
         pic[i] = malloc(YRES * sizeof(pixel));
     }
-    // Fill entire image with red
-    fill_rect(pic, 255, 0, 0, 0, 0, XRES, YRES);
+
+    // Let's just create a whole bunch of random rectangles of different sizes
+    // and colors
+
+    int devrand = open("/dev/urandom", O_RDONLY);
+    if (errno) {
+        printf("%s\n", strerror(errno));
+        return errno;
+    }
+
+    int shade_of_grey;
+    read(devrand, &shade_of_grey, sizeof(int));
+    shade_of_grey = shade_of_grey % 256;
+    fill_rect(pic, shade_of_grey, shade_of_grey, shade_of_grey, 0, 0, XRES, YRES);
+
+    int num_rects = 0;
+    read(devrand, &num_rects, sizeof(int));
+    num_rects = 10 + (num_rects % 6);
+
+    printf("Generating %d random rectangles\n", num_rects);
+
+    int params[7];
+    while (num_rects--) {
+        printf("%d rectangles left...\n", num_rects + 1);
+        for (i = 0; i < 7; i++) {
+            read(devrand, &params[i], sizeof(int));
+            params[i] = params[i] & 0x7FF; // Lighten the load
+        }
+        fill_rect(pic, params[0] % 256, params[1] % 256, params[2] % 256,
+                params[3] % XRES, params[4] % YRES, 60 + (params[5] % 241), 60 + (params[6] % 241));
+    }
+
     filewrite(FILENAME, pic2string(pic, XRES, YRES, MAX_C_VAL), get_size_of_buff(XRES, YRES, MAX_C_VAL));
     // Free the memory
     for (i = 0; i < XRES; i++) {
@@ -35,6 +69,12 @@ int main() {
     }
     free(pic);
     pic = NULL;
+
+    close(devrand);
+    if (errno) {
+        printf("%s\n", strerror(errno));
+        return errno;
+    }
     return 0;
 }
 
