84ef68b0076857f80b0cbb7d3b3a3aad0bbf2d7d
elc1798 <subliminalmau5@gmail.com>
2016-02-14 15:33:39 -0500

    Add octant locator

--- gfx_utils/draw.c
+++ gfx_utils/draw.c
@@ -16,6 +16,14 @@ void fill_rect(pixel **pic, int r, int g, int b, int x, int y, int dx, int dy) {
 void draw_line(pixel **pic, int r, int g, int b, int x0, int y0, int x1, int y1) {
     // Determine the octant without using divide
     int octant = -1;
-    if (x1 - x0 > 0 && y1 - y0 > 0) return;
+    if (x1 - x0 > 0 && y1 - y0 > 0) {
+        octant = (y1 - y0 > x1 - x0) ? 2 : 1;
+    } else if (x1 - x0 < 0 && y1 - y0 > 0) {
+        octant = (y1 - y0 > x0 - x1) ? 3 : 4;
+    } else if (x1 - x0 < 0 && y1 - y0 < 0) {
+        octant = (y0 - y1 > x0 - x1) ? 6 : 5;
+    } else if (x0 - x1 > 0 && y1 - y0 < 0) {
+        octant = (y0 - y1 > x1 - x0) ? 7 : 8;
+    }
 }
 
