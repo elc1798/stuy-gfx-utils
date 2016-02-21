36f079055fe94cf5109ab3f136ba5fb462cfd801
elc1798 <subliminalmau5@gmail.com>
2016-02-21 18:25:13 -0500

    Add picture generator

--- main.c
+++ main.c
@@ -18,36 +18,30 @@ int main() {
     YRES = 512;
     MAX_C_VAL = 255;
 
-    // An image is of type pixel**
-    printf("Creating a new picture\n");
     pixel **pic = new_picture(XRES, YRES);
 
-    // Fill entire image a white background
-    printf("Filling image with white background \n");
-    fill_rect(pic, new_pixel(255, 255, 255), new_point(0, 0), new_point(XRES, YRES));
+    fill_rect(pic, new_pixel(0, 0, 0), new_point(0, 0), new_point(XRES, YRES));
 
-    // Draw blue horizontal lines to test
-    printf("Drawing blue horizontal lines\n");
-    int i; for (i = 0; i < XRES; i += 5) {
-        draw_line(pic, new_pixel(0, 0, 255), new_point(0, i), new_point(i, i));
+    int red_val = 0;
+    int inc = 2;
+
+    int i; for (i = 0; i <= YRES; i += 4) {
+        draw_line(pic, new_pixel(red_val, 0, 255), new_point(0, YRES - i), new_point(i, i));
+        if (red_val + inc < 0 || red_val + inc > 255) {
+            inc *= -1;
+        }
+        red_val += inc;
     }
 
-    // Draw red vertical lines to test
-    printf("Drawing red vertical lines to test\n");
-    for (i = 0; i < YRES; i += 5) {
-        draw_line(pic, new_pixel(255, 0, 0), new_point(i, 0), new_point(i, i));
+    for (i = XRES; i >= 0; i -= 4) {
+        draw_line(pic, new_pixel(red_val, 0, 255), new_point(i, i), new_point(XRES - i, 0));
+        if (red_val + inc < 0 || red_val + inc > 255) {
+            inc *= -1;
+        }
+        red_val += inc;
     }
 
-    fill_rect(pic, new_pixel(0, 0, 0), new_point((XRES / 2) - 100, (YRES / 2) - 100),
-            new_point((XRES / 2) + 100, (YRES / 2) + 100));
-
-    // Oh boi~~~
-    for (i = (YRES / 2) - 100; i <= (YRES / 2) + 100; i += 10) {
-        draw_line(pic, new_pixel(0, 255, 0), new_point((XRES / 2), (YRES / 2)),
-                new_point((XRES / 2) + 100, i));
-        draw_line(pic, new_pixel(255, 255, 0), new_point((XRES / 2), (YRES / 2)),
-                new_point(-(XRES / 2) + 100, -i));
-    }
+    draw_line(pic, new_pixel(255, 0, 255), new_point(0, 0), new_point(XRES, YRES));
 
     // Write it out to an image
     char *s = pic2string(pic);
