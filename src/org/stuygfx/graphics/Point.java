c0bd6eaf84e4744cc4d6e870a03378a0bcdb28a2
elc1798 <subliminalmau5@gmail.com>
2016-03-27 14:27:15 -0400

    Fix formatting

--- src/org/stuygfx/graphics/Point.java
+++ src/org/stuygfx/graphics/Point.java
@@ -2,19 +2,19 @@ package org.stuygfx.graphics;
 
 public class Point {
 
-	public int x;
-	public int y;
-	public int z;
+    public int x;
+    public int y;
+    public int z;
 
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-		this.z = 0;
-	}
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.z = 0;
+    }
 
-	public Point(int x, int y, int z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
